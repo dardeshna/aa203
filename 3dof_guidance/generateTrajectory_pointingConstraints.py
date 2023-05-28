772de402e337b8f9e2513b76eb4ff4ce71a1a647
ndelurgio <delurgion@gmail.com>
2023-05-28 13:26:45 -0700

    add thrust pointing constraints to cvx program

--- 3dof_guidance/generateTrajectory_pointingConstraints.py
+++ 3dof_guidance/generateTrajectory_pointingConstraints.py
@@ -4,33 +4,33 @@ import matplotlib.pyplot as plt
 
 ## Constants
 g0 = 9.80665
-dry_mass = 1505
+dry_mass = 1700
 Isp = 225
 max_throttle = 0.8
-min_throttle = 0.3
+min_throttle = 0.2
 g = np.array([0,0,-3.7114]) # Mars Gravity
-T_max = 6*3100
-φ = 27*np.deg2rad(1) # Cant angle of thrusters (deg)
-nThruster = 1
-γ = 4*np.deg2rad(1) # glide slop constraint
-wet_mass = 1905
+T_max = 24000
+γ = 4*np.deg2rad(1) # glide slope constraint
+wet_mass = 2000
+n = np.array([0,0,1]) # Nominal thrust pointing vector
+θ = np.deg2rad(45) # Attitude constraint
 
 ## Initial Conditions
-r0 = 1000 * np.array([1.5, 0.1, 2])
-v0 = np.array([100,0.01,-75])
+r0 = np.array([450, -330, 2400])
+v0 = np.array([-40,10,-10])
 ## Terminal Conditions set to origin
 rf = np.array([0,0,0])
 vf = np.array([0,0,0])
 ## Time of Flight
-tf = 72
+tf = 57
 dt = 1
 N = int(np.ceil(tf/dt) + 1)
 ts = np.arange(0,tf,dt)
 
 ## Constraint Parameters
-α = 1/(Isp*g0*np.cos(φ))
-ρ1 = min_throttle*T_max*np.cos(φ)
-ρ2 = max_throttle*T_max*np.cos(φ)
+α = 5E-4
+ρ1 = min_throttle*T_max
+ρ2 = max_throttle*T_max
 
 #### Set up CVXPY Problem
 r = cp.Variable((N,3))
@@ -65,8 +65,10 @@ for i in range(N):
     μ1 = ρ1/z0_innerTerm
     μ2 = ρ2/z0_innerTerm
     constraints += [
-        # Thrust Constraint
+        # Thrust Magnitude Constraint
         cp.norm2(u[i]) <= σ[i],
+        # Thrust Pointing Constraint
+        n@u[i] >= np.cos(θ)*σ[i],
         # Enforce Sigma
         σ[i] >= μ1 * (1 - (z[i] - z0) + 1/2*cp.square(z[i]-z0)),
         σ[i] <= μ2 * (1 - (z[i] - z0)),
@@ -93,6 +95,7 @@ for i in range(N-1):
 ## Solve Problem
 prob = cp.Problem(objective,constraints)
 prob.solve()
+print(prob.status)
     
 m = np.exp(z.value)
 print(m)
@@ -103,7 +106,13 @@ ax.plot3D(r.value[:,0],r.value[:,1],r.value[:,2])
 ax.set_xlabel("X [m]")
 ax.set_ylabel("Y [m]")
 ax.set_zlabel("Z [m]")
-plt.savefig("figures/3dof_trajectory.png")
+plt.savefig("figures/3dof_trajectory_attitude.png")
+
+plt.figure()
+plt.plot(r.value[:,0],r.value[:,1])
+plt.xlabel("X [m]")
+plt.xlabel("Y [m]")
+plt.savefig("figures/3dof_surfacetrajectory_attitude.png")
 
 t_span = np.linspace(0,tf,N)
 plt.figure()
@@ -117,7 +126,7 @@ plt.subplot(3,1,3)
 plt.plot(t_span,r.value[:,2])
 plt.xlabel("t [s]")
 plt.ylabel("rz [m]")
-plt.savefig("figures/3dof_pos.png")
+plt.savefig("figures/3dof_pos_attitude.png")
 
 plt.figure()
 plt.subplot(3,1,1)
@@ -130,7 +139,7 @@ plt.subplot(3,1,3)
 plt.plot(t_span,v.value[:,2])
 plt.xlabel("t [s]")
 plt.ylabel("vz [m/s]")
-plt.savefig("figures/3dof_vel.png")
+plt.savefig("figures/3dof_vel_attitude.png")
 
 
 Tx = u.value[:,0] * m
@@ -169,4 +178,4 @@ plt.plot(t_span,m)
 plt.ticklabel_format(style='plain')
 plt.ylabel("m [kg]")
 plt.xlabel("t [s]")
-plt.savefig("figures/3dof_control.png")
+plt.savefig("figures/3dof_control_attitude.png")
